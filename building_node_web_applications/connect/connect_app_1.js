5d70989d52964c576d5a62920a70cb83aa2ef0cf
Ubuntu <ubuntu@ip-10-144-8-231.ec2.internal>
2013-07-31 15:40:19 +0000

    connect_app_1.js

---
+++ building_node_web_applications/connect/connect_app_1.js
@@ -0,0 +1,37 @@
+var connect = require('connect');
+var app = connect();
+// logger widdleware component //
+function logger(req, res, next) {
+  console.log('%s %s', req.method, req.url);
+  next();
+}
+//middleware componen that will send a response to the HTTP request //
+function hello(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
+}
+// A middleware component that performs HTTP Basic authentication //
+function restrict(req, res, next) {
+  var authorization = req.headers.authorization;
+  if (!authorization) {
+      return next(new Error('Unauthorized')); // the 'next' function is invoked with an Error object passed in as the argument. This notifies Connect that an application error has occured, which means that only error-handling middleware will be executed for the remainder of this HTTP request //
+  }
+  var parts = authorization.split(' ');
+  var scheme = parts[0];
+  var auth = new Buffer(parts[1], 'base64').toString().spli(':');
+  var user = auth[0];
+// A function that checks credentials against a database //
+  authenticateWithDatabase(user, pass, function(err) {
+    if (err) {
+      return next(err); // Inform dispatcher that an error occured //
+    }
+    next(); // Call next()) with no arguments when given valid credentials //
+  });
+}
+
+connect()
+  .use(logger)
+  .use('/admin', restrict)
+  .use('/admin', admin)
+  .use(hello)
+  .listen(3000);
